4f02dbb1d6ccbf3b7cd56565420d5b3dd5b58513
Ali Hassn <dev.alihsn@gmail.com>
2020-12-06 12:06:28 +0200

    my final project

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 $(function(){
     "use strict";
     // adjust silder header
-    let sliderH = $(".slider, carousel-item"),
+    let sliderH = $(".slider , .carousel-item"),
         winH = $(window).height(),
         upperH = $(".upper-bar").innerHeight(),
         navH = $(".navbar").innerHeight();
     sliderH.height(winH - (upperH + navH))
+
+    // featured work shuffle
+    $(".featured-work ul li").on("click",function(){
+        $(this).addClass("active").siblings().removeClass("active")
+        if($(this).data("class") === "All") {
+            $(".shuffle-images .col-md").css("opacity","1")
+        }else {
+            $(".shuffle-images .col-md").css("opacity",".2")
+            $($(this).data("class")).parent().css("opacity","1")
+        }
+    })
 });
